86ba550f4913e84626093813a2588f54014b89aa
developer <stevenmtune@gmail.com>
2020-09-24 12:11:00 -0700

    initial commit

---
+++ form.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Template Generator</title>
+	<link rel='stylesheet' href='style.css' />
+</head>
+<body>
+	<?php
+		$urlArr = [
+			'SECRETS AKUMAL RIVIERA MAYA',
+			'https://booking.secretsresorts.com/premium/index.html?id_albergo=13202&dc=7547&lingua_int=eng&id_stile=17409',
+			'DREAMS RIVIERA CANCUN RESORT & SPA',
+			'https://booking.dreamsresorts.com/premium/index.html?id_albergo=10577&dc=120&lingua_int=ita&id_stile=17493',
+			'NOW AMBER PUERTO VALLARTA',
+			'https://booking.nowresorts.com/premium/index.html?id_albergo=10342&dc=4353&lingua_int=fra&id_stile=17494'
+		];
+	?>
+
+	<form class='form' method='POST' action='results.php'>
+		<label class='form--label'>BE Type</label>
+		<select name='be-type' class='form--select form--element'>
+			<option class='form--option' value='advanced'>Advanced</option>
+			<option class='form--option' value='smart'>Smart</option>
+			<option class='form--option' value='premium'>Premium</option>
+		</select>
+		<label class='form--label' for='urls'>URL(s)</label>
+		<textarea class='form--textarea form--element' name='urls' id='urls'><?php echo implode(PHP_EOL,$urlArr); ?></textarea>
+		<input class='form--submit' type='submit'/>
+	</form>
+</body>
+</html>
+
+
+
---
+++ results.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Template Generator</title>
+	<link rel='stylesheet' href='style.css' />
+</head>
+<body>
+
+
+
+	<?php
+
+		//$propName = isset($_POST['property-name']) ? trim($_POST['property-name']) : '';
+
+		if(isset($_POST['urls']) && isset($_POST['be-type']))
+		{
+			$lineArr = explode(PHP_EOL, $_POST['urls']);
+
+			$urls = array_filter($lineArr, function($key){
+				return $key % 2 !== 0;
+			}, ARRAY_FILTER_USE_KEY);
+			
+			$propNames = array_filter($lineArr, function($key){
+				return $key % 2 === 0;
+			}, ARRAY_FILTER_USE_KEY);
+
+			for($i=0,$x=0,$y=1; $i<count($propNames); $i++,$x+=2,$y+=2)
+			{
+				printTemplate($propNames[$x], $urls[$y], $_POST['be-type']);
+			}
+		}
+
+		function printTemplate($propName, $url, $beType)
+		{
+			$url = str_replace('&tst_prntz=si', '', $url);
+			$getParams = str_replace('&', '<br>', substr($url, strpos($url, '?')+1));
+
+			switch($beType)
+			{
+				case 'advanced':
+					$actionUrlUri = '/reservations/risultato.html';
+					break;
+
+				case 'smart':
+					$actionUrlUri = '/reservation_hotel.htm';
+					break;
+
+				case 'premium':
+					$actionUrlUri = '/premium/index2.html';
+					break;
+
+				default:
+					$actionUrlUri = '/error';
+					break;
+			}
+
+			$actionUrl = substr($url, 0, strpos($url, '.com') + 4).$actionUrlUri;
+
+			$propName = ucwords(strtolower($propName));
+
+			$tpl = <<<EOT
+				${propName}<br>
+				${url}<br>
+				URL ACTION FORM ${actionUrl}<br>
+				${getParams}<br>
+				At the following link you can download the API document with the instructions on how to include Vertical Booking in the website.<br>
+				http://www.verticalbooking.com/downloads/Webmaster_API_eng_v3.0.pdf
+				<br><br>
+			EOT;
+
+			echo $tpl;
+		}
+	?>
+</body>
+</html>
